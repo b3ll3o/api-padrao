7a661609f96be73e57abf18feb90e8dc30a4afe7
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2025-09-06 20:53:05 -0300

    docs: Add ApiProperty decorators to Perfil and Permissao entities
    This commit adds `@ApiProperty` decorators to the `Perfil` (`src/perfis/domain/entities/perfil.entity.ts`) and `Permissao` (`src/permissoes/domain/entities/permissao.entity.ts`) entity classes.
    
    This enhancement provides more detailed and accurate schema definitions for these models in the generated Swagger documentation, improving API clarity and usability.

--- src/perfis/domain/entities/perfil.entity.ts
+++ src/perfis/domain/entities/perfil.entity.ts
@@ -1,7 +1,17 @@
 import { Permissao } from 'src/permissoes/domain/entities/permissao.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class Perfil {
+  @ApiProperty({ description: 'ID do perfil', example: 1 })
   id: number;
+
+  @ApiProperty({ description: 'Nome do perfil', example: 'Administrador' })
   nome: string;
+
+  @ApiProperty({
+    description: 'Permissões associadas ao perfil',
+    type: [Permissao],
+    required: false,
+  })
   permissoes?: Permissao[];
 }
--- src/permissoes/domain/entities/permissao.entity.ts
+++ src/permissoes/domain/entities/permissao.entity.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class Permissao {
+  @ApiProperty({ description: 'ID da permissão', example: 1 })
   id: number;
+
+  @ApiProperty({ description: 'Nome da permissão', example: 'read:users' })
   nome: string;
 }
