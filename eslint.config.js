4efb15638d65ee6a1e13b7265b77a4efa7ca1a68
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2025-09-08 16:08:38 -0300

    chore: atualiza configuração do eslint
    - Atualiza para o novo formato da v9 do ESLint
    - Adiciona globals para testes
    - Configura ambiente de módulos ES

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: typescriptParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        describe: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        jest: 'readonly'
+      },
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      prettier: prettier,
+    },
+    rules: {
+      ...typescript.configs['recommended'].rules,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
