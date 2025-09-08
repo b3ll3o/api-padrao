7e10c292835094599314e7e770508b29b24d3c17
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2025-09-08 13:21:36 -0300

    refactor: Remove unused import and method from AuthGuard

--- src/auth/application/guards/auth.guard.ts
+++ src/auth/application/guards/auth.guard.ts
@@ -1,8 +1,4 @@
-import {
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard as PassportAuthGuard } from '@nestjs/passport';
 import { Reflector } from '@nestjs/core';
 import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
@@ -38,13 +34,5 @@ export class AuthGuard extends PassportAuthGuard('jwt') {
       request.usuarioLogado = request.user; // user is attached by AuthGuard
     }
     return result as boolean;
-  }
-
-  handleRequest(err: any, user: any): any {
-    if (err || !user) {
-      throw err || new UnauthorizedException();
-    }
-    // The user is already attached to request.usuarioLogado in canActivate
-    return user;
   }
 }
